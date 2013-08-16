eddc1d9404d0a63e02c2bd8f12947711b6939a94
selim song <491497047@qq.com>
2013-08-16 17:08:32 +0800

    m

---
+++ mongo.php
@@ -0,0 +1,16 @@
+<?php
+$m = new mongoClient('mongodb://127.0.0.1', array());
+$db = $m->star;
+$collection = $db->users;
+$doc = array('messageId' => rand(0, 100), 'openid' => rand(0, 100), 'updateData' => date('d'));
+$collection->insert($doc);
+
+
+$cursor = $collection->find();
+
+foreach($cursor as $document){
+
+    print_r($document);
+	echo "<br />";
+
+}
